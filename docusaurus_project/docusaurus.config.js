7542c09cc4c641764891a06008dd79daa8df6273
Your Name <your-email@example.com>
2025-12-14 22:02:04 +0500

    fix: Update baseUrl to '/' for Vercel deployment

--- docusaurus_project/docusaurus.config.js
+++ docusaurus_project/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Humanoid Robotics & Physical AI Book',
   tagline: 'Building Intelligent Robots from Scratch',
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Ghulamullah-Khan-burdi.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'your-github-org', // Usually your GitHub org/user name.
-  projectName: 'humanoid-robotics-book', // Usually your repo name.
+  organizationName: 'Ghulamullah-Khan-burdi', // Usually your GitHub org/user name.
+  projectName: 'GIAIC-1Hackathon-Humanoid-AI-DrivenBook', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
